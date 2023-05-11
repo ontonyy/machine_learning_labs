a2b5872668465414bb0796d2606bcefbe35d15fe
Anton Gavrilin <anton.gavrilin@betpawa.com>
2023-05-12 02:22:59 +0300

    Reformat code for be more understandable and easy to exploring

--- image_classify/create_cnn_model.py
+++ image_classify/create_cnn_model.py
@@ -11,15 +11,11 @@ from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ReduceLROnPla
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-dataset_name = 'cifar10'
-dir = 'random_images'
+# Define hyperparameters
 batch_size = 128
-tensorboard_callback = TensorBoard(log_dir=f'logs/{dataset_name}')
 
-print("Approximately model creating will take ~10-15 minutes")
 n_epochs = int(input("Input how many epochs model will have: "))
 
-_, info = tfds.load(dataset_name, split=['train', 'test'], with_info=True)
 (train_data, train_labels), (test_data, test_labels) = cifar10.load_data()
 
 train_data = train_data / 255
@@ -27,74 +23,94 @@ test_data = test_data / 255
 
 train_labels, test_labels = train_labels.flatten(), test_labels.flatten()
 
-class_names = info.features["label"].names
-n_classes = info.features["label"].num_classes
-input_shape = info.features['image'].shape
 
-model = Sequential([
-    Conv2D(32, 3, padding='same', input_shape=input_shape, activation='relu'),
-    BatchNormalization(),
-    Conv2D(32, 3, activation='relu', padding='same'),
-    BatchNormalization(),
-    MaxPooling2D((2, 2)),
+def create_model():
+    return Sequential([
+        Conv2D(32, 3, padding='same', input_shape=(32, 32, 3), activation='relu'),
+        BatchNormalization(),
+        Conv2D(32, 3, activation='relu', padding='same'),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
 
-    Conv2D(64, 3, padding='same', activation='relu'),
-    BatchNormalization(),
-    Conv2D(64, 3, activation='relu', padding='same'),
-    BatchNormalization(),
-    MaxPooling2D((2, 2)),
+        Conv2D(64, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Conv2D(64, 3, activation='relu', padding='same'),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
 
-    Conv2D(128, 3, padding='same', activation='relu'),
-    BatchNormalization(),
-    Conv2D(128, 3, activation='relu', padding='same'),
-    BatchNormalization(),
-    MaxPooling2D((2, 2)),
+        Conv2D(128, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Conv2D(128, 3, activation='relu', padding='same'),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
 
-    Flatten(),
-    Dense(1024, activation='relu'),
-    Dropout(0.5),
-    Dense(512, activation='relu'),
-    Dropout(0.5),
-    Dense(n_classes, activation='softmax'),
-])
-
-val_loss_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15)
-val_accuracy_stop = EarlyStopping(monitor='val_acc', mode='min', verbose=1, patience=15)
-lr_schedule = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5)
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
-
-history = model.fit(train_data, train_labels, epochs=n_epochs,
-                 validation_data=(test_data, test_labels),
-                 callbacks=[tensorboard_callback, val_loss_stop, val_accuracy_stop, lr_schedule],
-                 batch_size=batch_size,
-                 verbose=1,
-                 steps_per_epoch=len(train_data) // batch_size,
-                 validation_steps=len(test_data) // batch_size)
-
-datagen = ImageDataGenerator(
-    rotation_range=15,
-    zoom_range=0.1,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    horizontal_flip=True,
-    vertical_flip=False
-)
-
-train_generator = datagen.flow(train_data, train_labels, batch_size=batch_size)
-test_generator = datagen.flow(test_data, test_labels, batch_size=batch_size)
-
-history2 = model.fit(train_generator, epochs=n_epochs,
-                 validation_data=test_generator,
-                 callbacks=[tensorboard_callback, val_loss_stop, val_accuracy_stop, lr_schedule],
-                 batch_size=batch_size,
-                 verbose=1,
-                 steps_per_epoch=len(train_data) // batch_size,
-                 validation_steps=len(test_data) // batch_size)
-
-loss, accuracy = model.evaluate(test_data, test_labels)
-print(f"Loss: {loss}, Accuracy: {accuracy}")
-
-model.save("image_cnn_model.h5", include_optimizer=True)
+        Flatten(),
+        Dense(1024, activation='relu'),
+        Dropout(0.5),
+        Dense(512, activation='relu'),
+        Dropout(0.5),
+        Dense(10, activation='softmax'),
+    ])
 
 
+def create_callbacks():
+    tensorboard_callback = TensorBoard(log_dir=f'logs/cifar10')
+    val_loss_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15)
+    val_accuracy_stop = EarlyStopping(monitor='val_acc', mode='min', verbose=1, patience=15)
+    lr_schedule = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5)
+    return [tensorboard_callback, val_loss_stop, val_accuracy_stop, lr_schedule]
+
+
+def compile_model():
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+
+
+def train_model_simple():
+    model.fit(train_data, train_labels, epochs=n_epochs,
+              validation_data=(test_data, test_labels),
+              callbacks=create_callbacks(),
+              batch_size=batch_size,
+              verbose=1,
+              steps_per_epoch=len(train_data) // batch_size,
+              validation_steps=len(test_data) // batch_size)
+
+
+def train_model_with_augmentation():
+    datagen = ImageDataGenerator(
+        rotation_range=15,
+        zoom_range=0.1,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True,
+        vertical_flip=False
+    )
+
+    train_generator = datagen.flow(train_data, train_labels, batch_size=batch_size)
+    test_generator = datagen.flow(test_data, test_labels, batch_size=batch_size)
+
+    model.fit(train_generator, epochs=n_epochs,
+              validation_data=test_generator,
+              callbacks=create_callbacks(),
+              batch_size=batch_size,
+              verbose=1,
+              steps_per_epoch=len(train_data) // batch_size,
+              validation_steps=len(test_data) // batch_size)
+
+
+def evaluate():
+    loss, accuracy = model.evaluate(test_data, test_labels)
+    print(f"Loss: {loss}, Accuracy: {accuracy}")
+
+
+def save_model():
+    model.save("image_cnn_model.h5", include_optimizer=True)
+
+
+if __name__ == '__main__':
+    print("Approximately model creating will take ~10-15 minutes")
+    model = create_model()
+    compile_model()
+    train_model_simple()
+    train_model_with_augmentation()
+    evaluate()
+    save_model()
--- image_classify/image_classify.py
+++ image_classify/image_classify.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 
-dir = 'random_images'
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 model = load_model("image_cnn_model.h5")
 
 image_path = input("Input image for prediction(with format .jpg/.png/.jpeg): ")
+
 
 def get_top_predictions(prediction):
     top_probs_indices = np.argsort(prediction)[0][::-1][:10]
@@ -16,25 +16,30 @@ def get_top_predictions(prediction):
     return top_class_names, top_probs
 
 
-plot_img = cv.imread(f"{image_path}")
-img = plot_img.astype(float)
+def predict_visualize_image():
+    plot_img = cv.imread(f"{image_path}")
+    img = plot_img.astype(float)
 
-img = cv.resize(img, (32, 32))
-img = np.expand_dims(img, axis=0) / 255.0
+    img = cv.resize(img, (32, 32))
+    img = np.expand_dims(img, axis=0) / 255.0
 
-prediction = model.predict(img)
-index = np.argmax(prediction)
+    prediction = model.predict(img)
+    index = np.argmax(prediction)
 
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4))
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4))
 
-ax1.set_title(f"Truly is {image_path.split('.')[0]}\nPrediction is {class_names[index]}")
-ax1.imshow(cv.cvtColor(plot_img, cv.COLOR_BGR2RGB))
-ax1.axis("off")
+    ax1.set_title(f"Truly is {image_path.split('.')[0]}\nPrediction is {class_names[index]}")
+    ax1.imshow(cv.cvtColor(plot_img, cv.COLOR_BGR2RGB))
+    ax1.axis("off")
 
-top_names, top_predictions = get_top_predictions(prediction)
-ax2.barh(top_names, top_predictions)
-ax2.set_xlabel("Probability")
-ax2.set_xlim(0, 1)
+    top_names, top_predictions = get_top_predictions(prediction)
+    ax2.barh(top_names, top_predictions)
+    ax2.set_xlabel("Probability")
+    ax2.set_xlim(0, 1)
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    predict_visualize_image()
